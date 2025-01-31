769f3133e84544a6921d5b40cae7d217ec362597
SamyakBorkar <2022bit037@sggs.ac.in>
2025-02-01 00:02:24 +0530

    List Land Page Added

--- client/Parking-App/vite.config.js
+++ client/Parking-App/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from "fs"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -15,5 +16,7 @@ import fs from "fs"
 // })
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss()
+  ],
 })
